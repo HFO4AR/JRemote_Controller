46390b91847267215c05b1a9a411177897d0908f
HFO4AR <amikono728@gmail.com>
2026-03-04 00:34:04 +0800

    fix: 移除重复的导入

--- android_app/app/src/main/java/com/example/jremote/screen/ConnectionScreen.kt
+++ android_app/app/src/main/java/com/example/jremote/screen/ConnectionScreen.kt
@@ -40,14 +40,9 @@ import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pullrefresh.PullRefreshIndicator
 import androidx.compose.foundation.pullrefresh.pullRefresh
 import androidx.compose.foundation.pullrefresh.rememberPullRefreshState
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
